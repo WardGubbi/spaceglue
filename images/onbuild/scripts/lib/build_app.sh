3a60fbd717c8c5de495075efa66685bd28d2d097
Ward Gubbi <ward.gubbi@23seconds.be>
2017-02-28 14:15:57 +0100

    server only with specific meteor version

--- images/onbuild/scripts/lib/build_app.sh
+++ images/onbuild/scripts/lib/build_app.sh
@@ -44,41 +44,6 @@ if ! [ -f ".meteor/release" ]; then
   exit 1
 fi
 
-# First, try to get the Meteor version from the .meteor/release file in the app.
-if [ -z "$METEOR_RELEASE" ]; then
-  METEOR_RELEASE="$(grep "^METEOR@" .meteor/release | sed 's/^METEOR@//;')"
-fi
-
-# Check to make sure it's not a generally unpublished version, like beta or RC.
-# These aren't generally available as direct bootstrap downloads.
-if ! [ -z "$METEOR_RELEASE" ]; then
-  if (echo "$METEOR_RELEASE" | grep -Eq '\-(alpha|beta|rc)'); then
-    echo "=> [warn] Beta and RC releases cannot use the streamlined downloading..."
-    unset METEOR_RELEASE
-  fi
-fi
-
-# Would like to use a cached Meteor here at some point, but for now,
-# download the installer, attempting to use the preferred version, from
-# the install.meteor.com script
-if true; then
-  curl -sL "https://install.meteor.com/?release=${METEOR_RELEASE}" \
-    > /tmp/install_meteor.sh
-
-  if [ -z "${METEOR_RELEASE}" ]; then
-    # Read it from the install file.
-    echo "Setting METEOR_RELEASE from the installer"
-    eval "METEOR_RELEASE=$( \
-      cat /tmp/install_meteor.sh | \
-      grep '^RELEASE="[0-9\.a-z-]\+"$' | \
-      sed 's/RELEASE=//;s/"//g' \
-    )"
-  fi
-
-  echo "=> Running the ${METEOR_RELEASE} installer..."
-  cat /tmp/install_meteor.sh | sed s/--progress-bar/-sL/g | /bin/sh
-fi
-
 # Useful for various hot-patches/optimizations
 meteor_bin="$HOME/.meteor/meteor"
 meteor_bin_symlink="$(readlink $meteor_bin)"
